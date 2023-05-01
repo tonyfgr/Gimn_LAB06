926aa7991b39acc5408fe25a44383f87c3a49d46
tonyfgr <antony.gonzales@tecsup.edu.pe>
2023-05-01 00:08:30 -0500

    Se añade Lab06

---
+++ registro.php
@@ -0,0 +1,58 @@
+<?php include 'template/header.php' ?>
+
+<div class="col-md-4">
+            <div class="card" style="justify-content: center">
+                <div class="card-header">
+                    Ingresar datos:
+                </div>
+                <form class="p-4" method="POST" action="registrar.php">
+                    <div class="mb-3">
+                        <label class="form-label">Nombres completos: </label>
+                        <input type="text" class="form-control" name="txtNombres" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Fecha nacimiento: </label>
+                        <input type="date" class="form-control" name="txtFecha" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Genero: </label>
+                        <input type="text" class="form-control" name="txtGenero" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">email: </label>
+                        <input type="text" class="form-control" name="txtEmail" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Contraseña: </label>
+                        <input type="text" class="form-control" name="txtContra" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Direccion: </label>
+                        <input type="text" class="form-control" name="txtDireccion" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Ciudad: </label>
+                        <input type="text" class="form-control" name="txtCiudad" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Estado: </label>
+                        <input type="text" class="form-control" name="txtEstado" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Codigo Postal: </label>
+                        <input type="text" class="form-control" name="txtCodigo" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Telefono: </label>
+                        <input type="text" class="form-control" name="txtTelefono" autofocus required>
+                    </div>
+                    <div class="d-grid">
+                        <input type="hidden" name="oculto" value="1">
+                        <input type="submit" class="btn btn-primary" value="Registrar">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
---
+++ template/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>CRUD php y mysql b5</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- cdn icnonos-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Dongle:wght@300&family=Nunito:wght@600;800&family=Roboto+Flex:wght@200;300;400;500&family=Roboto+Serif:ital,wght@0,200;1,200;1,300;1,400;1,900&family=Roboto+Slab:wght@100;200;300;500&family=Roboto:ital,wght@1,300;1,500;1,700&family=Sofia&display=swap');
+      </style>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Dongle:wght@300&family=Nunito:wght@600;800&family=Overpass:wght@200&family=Roboto+Flex:wght@200;300;400;500&family=Roboto+Serif:ital,wght@0,200;1,200;1,300;1,400;1,900&family=Roboto+Slab:wght@100;200;300;500&family=Roboto:ital,wght@1,300;1,500;1,700&family=Sofia&display=swap');
+      </style>
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/navbar.css" />
+  </head>
+  <body>
+  <header class="topheader">
+        <nav class="topnav">
+          <a href="index.php" class="logo">
+            <img height="50" width="125" src="./fithub.svg" alt="Fithub" />
+          </a>
+          <ul class="menu">
+            <li><a href="registro.php" target="_self">Registrate</a></li>
+            <li><a href="lista.php" target="_self">Lista de Usuarios</a></li>
+          </ul>
+        </nav>
+      </header>
